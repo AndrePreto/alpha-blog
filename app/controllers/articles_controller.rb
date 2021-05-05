60303a2ba171757d43a4ae5d433d5e136398ab10
AndrePreto <a.preto@campus.fct.unl.pt>
2021-05-05 20:41:10 +0100

    Articles Edit and Destroy routes and corresponding views and actions. Layout paths

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, olny: [:show]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
   def index
@@ -11,9 +11,47 @@ class ArticlesController < ApplicationController
     @article
   end
 
+  #GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  #POST /articles
+  def create
+    @article = Article.create(article_params)
+    if @article.persisted?
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  #GET /articles/:id/edit
+  def edit; end
+
+  #PUT /article/:id/update
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def set_article
     @article = Article.find_by(id: params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:index, :show]
+  resources :articles #, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
