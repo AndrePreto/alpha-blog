e125a527163cf5029a0856161a328e44755efdb2
AndrePreto <a.preto@campus.fct.unl.pt>
2021-05-13 23:02:59 +0100

    feature/user-model-validations - Added username and email validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false }, 
+                       length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false }, 
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+                    
 end
