15dbbcb1884bd6208187ab0fd29f5f434b5e6ecb
AndrePreto <a.preto@campus.fct.unl.pt>
2021-05-14 09:16:22 +0100

    feature/user-articles-association - Add one-to-many association between users and articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   
+  # GET / -> root
   def home
     @articles = Article.limit(3).order("RANDOM()")
   end
 
+  # GET /about
   def about; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles
+  
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false }, 
                        length: { minimum: 3, maximum: 25 }
@@ -7,5 +9,4 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }, 
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
-                    
 end
